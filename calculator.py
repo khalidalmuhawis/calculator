5086d3eca4f6fdf0a1c8d090828c653e8d7175bc
khalid almuhawis <khalidalmuhawis0@gmail.com>
2020-08-17 11:50:59 +0300

    our first calculator

--- calculator.py
+++ calculator.py
@@ -1,10 +1,30 @@
 
 def main():
-	#write your code here
-	pass
-	
+    first_number = raw_input("Enter the first number: ")
+    second_number = raw_input("Enter the second number: ")
+    operation = raw_input("Choose an operation: ")
+
+    if first_number.isdigit() and second_number.isdigit():
+        first_number = int(first_number)
+        second_number = int(second_number)
+        if operation == "+":
+            print(first_number + second_number)
+        elif operation == "-":
+            print(first_number - second_number)
+        elif operation == "/":
+            print(first_number / second_number)
+        elif operation == "*":
+            print(first_number * second_number)
+        else:
+            print("invalid operation")
+
+    else:
+        print("invalid numbers")
+
+    pass
+
 
 
 
 if __name__ == '__main__':
-	main()
+    main()
